4e6421e362988b4b8541213e5fa396088ca8f1a7
Andy Ferris <ferris.andy@gmail.com>
2019-11-03 14:14:34 +1000

    Fixed 32-bit tests

--- test/PairDictionary.jl
+++ test/PairDictionary.jl
@@ -2,8 +2,8 @@
     h = HashIndices{Int64}([3,6,9])
     p = pairs(h)
     @test p isa AbstractDictionary{Int64, Pair{Int64, Int64}}
-    @test p[3] === (3 => 3)
-    @test p[6] === (6 => 6)
-    @test p[9] === (9 => 9)
-    @test length(p) === 3
+    @test p[3] == (3 => 3)
+    @test p[6] == (6 => 6)
+    @test p[9] == (9 => 9)
+    @test length(p) == 3
 end
