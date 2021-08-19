7e96aba6ea337c5a5a4ba58fa11cde5d6fcaa18b
Chris Foster <chris42f@gmail.com>
2021-08-19 23:52:03 +1000

    Ensure only.(d) test is run without --bounds-check

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,3 +16,8 @@ include("filter.jl")
 include("find.jl")
 include("reverse.jl")
 include("show.jl")
+
+# Run the following test without julia --check-bounds=yes mode
+cmd = deepcopy(Base.julia_cmd())
+filter!(a->!startswith(a, "--check-bounds="), cmd.exec)
+@test process_exited(run(`$cmd auto_boundscheck.jl`))
