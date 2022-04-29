6209544984abd9459f67bea885e5e814cc968c81
Matthew Fishman <mfishman@flatironinstitute.org>
2022-04-29 15:39:35 -0400

    Fix ArrayDictionary test

--- test/ArrayDictionary.jl
+++ test/ArrayDictionary.jl
@@ -84,7 +84,7 @@
 
         dictcopy = copy(dict)
         @test dict isa ArrayDictionary{Int64, String}
-        @test sharetokens(dict, dictcopy)
+        @test !sharetokens(dict, dictcopy)
         if VERSION < v"1.6-"
             io = IOBuffer(); show(io, MIME"text/plain"(), dict); @test String(take!(io)) == "2-element ArrayDictionary{Int64,String,ArrayIndices{Int64,Array{Int64,1}},Array{String,1}}\n 1 │ #undef\n 2 │ #undef"
         else
